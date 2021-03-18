3c6f18d779c57260ca0bff8bb1d28a74c1014c11
MatthewTGM <matthewtgm120@gmail.com>
2021-03-18 23:17:03 +0200

    Update ConfigEntry.java

--- src/main/java/ga/matthewtgm/tgmconfig/ConfigEntry.java
+++ src/main/java/ga/matthewtgm/tgmconfig/ConfigEntry.java
@@ -5,12 +5,12 @@ import lombok.Setter;
 
 public class ConfigEntry<T> {
 
-    @Getter @Setter private String name;
-    @Getter @Setter private T value;
-
     public ConfigEntry(String name, T value) {
         this.name = name;
         this.value = value;
     }
+
+    @Getter @Setter private String name;
+    @Getter @Setter private T value;
 
 }
