4f90f1defb3860717b59b7bd210a1dfd9b454828
MatthewTGM <matthewtgm120@gmail.com>
2021-05-27 13:03:35 +0200

    Added a lot of Javadocs and made saving write the JSON as "pretty"

--- src/main/java/xyz/matthewtgm/tgmconfig/TGMConfig.java
+++ src/main/java/xyz/matthewtgm/tgmconfig/TGMConfig.java
@@ -41,10 +41,16 @@ public class TGMConfig {
         if (!(new File(directory, name + ".json")).exists()) save();
     }
 
+    /**
+     * @return a deep copy of the config.
+     */
     public TGMConfig clone() {
         return new TGMConfig(name, directory, configObj);
     }
 
+    /**
+     * Syncs the class with the saved config.
+     */
     public void sync() {
         try {
             JsonObject updated = JsonReader.readObj(name, directory);
@@ -55,99 +61,175 @@ public class TGMConfig {
         }
     }
 
+    /**
+     * @return Whether or not the class is synced with the saved config.
+     */
     public boolean isSynced() {
         JsonObject saved = JsonReader.readObj(name, directory);
         return configObj.equals(saved);
     }
 
+    /**
+     * Saves the config, the saved config JSON is "pretty".
+     */
     public void save() {
+        JsonWriter.writeObj(name, configObj, directory, true);
+    }
+
+    /**
+     * Saves the config with raw JSON, no indents.
+     */
+    public void rawSave() {
         JsonWriter.writeObj(name, configObj, directory);
     }
 
+    /**
+     * Adds an entry to the config.
+     * @param entry The entry to add.
+     */
     public TGMConfig add(ConfigEntry<?> entry) {
         configObj.put(entry.getName(), entry.getValue());
         return this;
     }
 
+    /**
+     * Syncs the config then adds the entry.
+     * @param entry The entry to add.
+     */
     public TGMConfig syncAndAdd(ConfigEntry<?> entry) {
         sync();
         add(entry);
         return this;
     }
 
+    /**
+     * Adds the entry then saves to a file.
+     * @param entry The entry to add.
+     */
     public TGMConfig addAndSave(ConfigEntry<?> entry) {
         configObj.put(entry.getName(), entry.getValue());
         save();
         return this;
     }
 
+    /**
+     * Adds all entries from the map to the config JSON.
+     * @param map The map of which's entries to add.
+     */
     public TGMConfig addAll(Map<? extends String, ?> map) {
         configObj.putAll(map);
         return this;
     }
 
+    /**
+     * Adds all entries from the JsonObject to the config JSON.
+     * @param jsonObject The object of which's entries to add.
+     */
     public TGMConfig addAll(JsonObject jsonObject) {
         configObj.putAll(jsonObject);
         return this;
     }
 
+    /**
+     * @param config The config of which's entries to add.
+     */
     public TGMConfig addAll(TGMConfig config) {
         configObj.putAll(config.getConfigObj());
         return this;
     }
 
+    /**
+     * Syncs the config then adds all entries in the map.
+     * @param map The map of which's entries to add.
+     */
     public TGMConfig syncAndAddAll(Map<? extends String, ?> map) {
         sync();
         addAll(map);
         return this;
     }
 
+    /**
+     * Syncs the config then adds all the entries in the JsonObject.
+     * @param json The object of which's entries to add.
+     */
     public TGMConfig syncAndAddAll(JsonObject json) {
         sync();
         addAll(json);
         return this;
     }
 
+    /**
+     * Syncs the config then adds all entries in the config provided.
+     * @param config The config of which's entries to add.
+     */
     public TGMConfig syncAndAddAll(TGMConfig config) {
         sync();
         addAll(config.getConfigObj());
         return this;
     }
 
+    /**
+     * Adds all the entries in the map then saves.
+     * @param map The map of which's entries to add.
+     */
     public TGMConfig addAllAndSave(Map<? extends String, ?> map) {
         addAll(map);
         save();
         return this;
     }
 
+    /**
+     * Adds all entries in the JsonObject then saves.
+     * @param jsonObject The object of which's entries to add.
+     */
     public TGMConfig addAllAndSave(JsonObject jsonObject) {
         addAll(jsonObject);
         save();
         return this;
     }
 
+    /**
+     * Adds all entries in the provided config then saves.
+     * @param config The config of which's entries to add.
+     */
     public TGMConfig addAllAndSave(TGMConfig config) {
         addAll(config);
         save();
         return this;
     }
 
+    /**
+     * Adds the entry if it is absent.
+     * @param entry The entry to add.
+     */
     public TGMConfig addIfAbsent(ConfigEntry<?> entry) {
         configObj.putIfAbsent(entry.getName(), entry.getValue());
         return this;
     }
 
+    /**
+     * Adds the entry if it is absent, then saves.
+     * @param entry The entry to add.
+     */
     public TGMConfig addIfAbsentAndSave(ConfigEntry<?> entry) {
         configObj.putIfAbsent(entry.getName(), entry.getValue());
         save();
         return this;
     }
 
+    /**
+     * Replaces the entry if it's already present.
+     * @param entry The entry to replace.
+     */
     public TGMConfig replace(ConfigEntry<?> entry) {
         configObj.replace(entry.getName(), entry.getValue());
         return this;
     }
 
+    /**
+     * Replaces the entry and saves.
+     * @param entry The entry to replace.
+     */
     public TGMConfig replaceAndSave(ConfigEntry<?> entry) {
         configObj.replace(entry.getName(), entry.getValue());
         save();
@@ -165,12 +247,20 @@ public class TGMConfig {
         return this;
     }
 
+    /**
+     * Removes the entry provided.
+     * @param entry The entry to remove.
+     */
     public TGMConfig remove(ConfigEntry<?> entry) {
         if (entry.getValue() instanceof String && ((String) entry.getValue()).isEmpty()) configObj.remove(entry.getName());
         else configObj.remove(entry.getName(), entry.getValue());
         return this;
     }
 
+    /**
+     * Removes the entry provided then saves.
+     * @param entry The entry to remove.
+     */
     public TGMConfig removeAndSave(ConfigEntry<?> entry) {
         if (entry.getValue() instanceof String && ((String) entry.getValue()).isEmpty()) configObj.remove(entry.getName());
         else configObj.remove(entry.getName(), entry.getValue());
@@ -178,10 +268,18 @@ public class TGMConfig {
         return this;
     }
 
+    /**
+     * Removes the entry provided.
+     * @param key The key of the entry you want to remove.
+     */
     public TGMConfig remove(String key) {
         return remove(new ConfigEntry<>(key, ""));
     }
 
+    /**
+     * Removes the entry provided then saves.
+     * @param key The key of the entry you want to remove.
+     */
     public TGMConfig removeAndSave(String key) {
         return removeAndSave(new ConfigEntry<>(key, ""));
     }
@@ -201,7 +299,11 @@ public class TGMConfig {
         return this;
     }
 
-    public TGMConfig forEachEntry(BiConsumer<? super String, ? super Object> action) {
+    /**
+     * Loops through each entry and runs the action provided.
+     * @param action The action to run for each entry.
+     */
+    public TGMConfig forEach(BiConsumer<? super String, ? super Object> action) {
         configObj.forEach(action);
         return this;
     }
@@ -217,59 +319,103 @@ public class TGMConfig {
         return this;
     }
 
+    /**
+     * @return The amount of entries.
+     */
     public int size() {
         return configObj.size();
     }
 
+    /**
+     * @return Whether or not the config JSON has any entries or not.
+     */
     public boolean isEmpty() {
         return configObj.isEmpty();
     }
 
+    /**
+     * @param key The key to check for.
+     * @return Whether or not the config JSON contains an entry with the key specified.
+     */
     public boolean containsKey(String key) {
         return configObj.containsKey(key);
     }
 
+    /**
+     * @param value The value to check for.
+     * @return Whether or not the config JSON contains an entry with the value specified.
+     */
     public boolean containsValue(Object value) {
         return configObj.containsValue(value);
     }
 
+    /**
+     * @param key The item to check for.
+     * @return Whether or not the config JSON contains the key provided as a key or a value.
+     */
     public boolean contains(Object key) {
         return containsKey(key.toString()) || containsValue(key);
     }
 
+    /**
+     * Clears the config JSON.
+     */
     public TGMConfig clear() {
         configObj.clear();
         return this;
     }
 
+    /**
+     * Clears the config JSON if the key isn't present.
+     * @param key The key to check for.
+     */
     public TGMConfig clearIfAbsent(String key) {
         if (!containsKey(key)) clear();
         return this;
     }
 
+    /**
+     * Clears the config JSON if the key is present.
+     * @param key The key to check for.
+     */
     public TGMConfig clearIfPresent(String key) {
         if (containsKey(key)) clear();
         return this;
     }
 
+    /**
+     * Clears the config JSON and saves.
+     */
     public TGMConfig clearAndSave() {
         clear();
         save();
         return this;
     }
 
+    /**
+     * Clears the config JSON if the key isn't present, then saves.
+     * @param key The key to check for.
+     */
     public TGMConfig clearIfAbsentAndSave(String key) {
         if (!containsKey(key)) clear();
         save();
         return this;
     }
 
+    /**
+     * Clears the config JSON if the key is present, then saves.
+     * @param key The key to check for.
+     */
     public TGMConfig clearIfPresentAndSave(String key) {
         if (containsKey(key)) clear();
         save();
         return this;
     }
 
+    /**
+     * Clears the config JSON then adds the entry.
+     * @param entry The entry to add.
+     */
     public TGMConfig clearAndAdd(ConfigEntry<?> entry) {
         clear();
         add(entry);
@@ -294,14 +440,23 @@ public class TGMConfig {
         return this;
     }
 
+    /**
+     * @return All the entry values in the config JSON.
+     */
     public Collection<Object> values() {
         return configObj.values();
     }
 
+    /**
+     * @return All keys in the config JSON.
+     */
     public Set<String> keySet() {
         return configObj.keySet();
     }
 
+    /**
+     * @return All the entries in the config JSON.
+     */
     public Set<Map.Entry<String, Object>> entrySet() {
         return configObj.entrySet();
     }
@@ -387,6 +542,10 @@ public class TGMConfig {
 
     public JsonObject getConfigObj() {
         return configObj;
+    }
+
+    public String toString() {
+        return toJson();
     }
 
 }
